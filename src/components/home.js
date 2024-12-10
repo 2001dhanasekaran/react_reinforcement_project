7eb08599121717abafe72d0164aa8daa3ce0ab37
Dhanasekaran <2001.dhanasekaran@gmail.com>
2024-12-10 09:26:31 +0530

    button issue fixed

--- src/components/home.js
+++ src/components/home.js
@@ -10,7 +10,7 @@ export default function Home(){
             <h1 data-aos="fade-down" style={{color:'rgb(122, 64, 177)'}}>Dhanasekaran</h1>
             <h2 data-aos="fade-down">Expert Front-end Developer</h2>
             <h4 data-aos="fade-down">Building functional and visually stunning web experiences.</h4>
-            <Link data-aos="fade-up" className="btn btn-outline-primary btm-lg" to="/contact" >Contact Me</Link>
+            <Link data-aos="fade-up" className="btn btn-outline-primary" to="/contact" >Contact Me</Link>
             <About />
             <Projects />
             <Contact />
