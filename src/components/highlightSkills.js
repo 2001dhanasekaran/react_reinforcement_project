3b3868d630b5cb925c579722206718eac39eb603
Dhanasekaran <2001.dhanasekaran@gmail.com>
2025-12-07 15:57:19 +0530

    update HighlightSkills component layout and adjust button margin; refine color scheme in styles

--- src/components/highlightSkills.js
+++ src/components/highlightSkills.js
@@ -2,7 +2,7 @@ import './style.css';
 export default function HighlightSkills(props) {
     const { skills } = props;
     return (
-        <div className="d-flex mb-3">
+        <div className="d-flex flex-wrap mb-4">
             {skills.map((skill, index) => (
                 <div key={index} className="me-2 mb-2 px-2 skills-box">
                     {skill}
--- src/components/home.js
+++ src/components/home.js
@@ -10,7 +10,7 @@ export default function Home(){
                 <h1 data-aos="fade-down" style={{color:'rgb(122, 64, 177)'}}>Dhanasekaran</h1>
                 <h2 data-aos="fade-down">Expert MERN Stack Developer</h2>
                 <h4 data-aos="fade-down">Building functional and visually stunning web experiences.</h4>
-                <a data-aos="fade-up" className="btn btn-outline-primary" href="#contact" >Contact Me</a> 
+                <a data-aos="fade-up" className="btn btn-outline-primary mt-3" href="#contact" >Contact Me</a> 
             </div>
             <div id="about" className='main-section'>
                <About />
